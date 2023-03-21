115112c8cf2f6b4db5d2478c8fcf28288cb3b3ad
Namix23 <98861500+Namix23@users.noreply.github.com>
2023-03-21 01:19:26 -0400

    Create project table and stats

--- sql_dbparts_tables.sql
+++ sql_dbparts_tables.sql
@@ -13,3 +13,8 @@ DESCRIBE part;
 I modify the column data type inside the table accordingly*/
 ALTER TABLE part MODIFY COLUMN p_id VARCHAR(50);
 INSERT INTO part(p_id,pname,colour,weight,city) VALUES ('p1','nut','red',12,'london'),('p2','bolt','green',17,'paris'),('p3','screw','blue',17,'rome'),('p4','screw','red',14,'london'),('p5','cam','blue',12,'paris'),('p6','cog','red',19,'london');
+/*I create second table called project
+and insert stats inside */
+USE parts;
+CREATE TABLE project(j_id VARCHAR(50),jname VARCHAR(50),city VARCHAR(50));
+INSERT INTO project(j_id,jname,city) VALUES ('j1','sorter','paris'),('j2','display','rome'),('j3','ocr','athens'),('j4','console','athens'),('j5','raid','london'),('j6','eds','oslo'),('j7','tape','london');
