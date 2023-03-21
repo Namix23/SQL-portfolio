41b13fbb71a2469a7c12e294f8f1717e13973581
Namix23 <98861500+Namix23@users.noreply.github.com>
2023-03-21 00:59:45 -0400

    Create parts database and part table

---
+++ sql_dbparts_tables.sql
@@ -0,0 +1,15 @@
+/*I create a parts database and a table. 
+Then I insert the parts data into the part table*/
+CREATE DATABASE parts;
+CREATE TABLE part(p_id INT,pname VARCHAR(50),colour VARCHAR(50),weight INT,city VARCHAR(50));
+USE parts;
+CREATE TABLE part(p_id INT,pname VARCHAR(50),colour VARCHAR(50),weight INT,city VARCHAR(50));
+INSERT INTO part(p_id,pname,colour,weight,city) VALUES ('p1','nut','red',12,'london'),('p2','bolt','green',17,'paris'),('p3','screw','blue',17,'rome'),('p4','screw','red',14,'london'),('p5','cam','blue',12,'paris'),('p6','cog','red',19,'london');
+/*After some time of no progress in row 1 for p1,
+I check to see if the column even exists in the part table*/
+INSERT INTO part(p_id,pname,colour,weight,city) VALUES (p1,'nut','red',12,'london'),(p2,'bolt','green',17,'paris'),(p3,'screw','blue',17,'rome'),(p4,'screw','red',14,'london'),(p5,'cam','blue',12,'paris'),(p6,'cog','red',19,'london');
+DESCRIBE part;
+/*I realize p_id has the wrong data type, so
+I modify the column data type inside the table accordingly*/
+ALTER TABLE part MODIFY COLUMN p_id VARCHAR(50);
+INSERT INTO part(p_id,pname,colour,weight,city) VALUES ('p1','nut','red',12,'london'),('p2','bolt','green',17,'paris'),('p3','screw','blue',17,'rome'),('p4','screw','red',14,'london'),('p5','cam','blue',12,'paris'),('p6','cog','red',19,'london');
