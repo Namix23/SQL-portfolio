f267dffb4df2d2c93bafd95ae83bd9499eefb9e8
Namix23 <98861500+Namix23@users.noreply.github.com>
2023-04-19 20:32:10 -0400

    Add column, row data for groceries table

--- sql_bodega.sql
+++ sql_bodega.sql
@@ -1,7 +1,25 @@
-/*Create database and grocery list: bananas (4), peanut butter (1), dark chocolate bars (2)*/
+--Create grocery list: bananas (4), peanut butter (1), dark chocolate bars (2)
 CREATE DATABASE bodega;
 CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INT);  
 INSERT INTO groceries VALUES (1, 'bananas', 4), (2, 'peanut butter', 1), (3, 'dark chocolate bars', 2);
-SELECT * FROM groceries;
-/*Add aisle column inside groceries table and update existing rows*/
-ALTER TABLE groceries ADD (aisle INT);
+--Realize I wanted to add 3 more rows and add an aisle column 
+INSERT INTO groceries VALUES 
+(4, 'ice cream', 1),
+(5, 'cherries', 6),
+(6, 'chocolate syrup', 1);
+ALTER TABLE groceries ADD aisle INT;
+--Update the missing rows under the aisle column
+UPDATE groceries
+SET aisle= 7 WHERE id= 1;
+UPDATE groceries
+SET aisle= 2 WHERE id= 2;
+UPDATE groceries
+SET aisle= 2 WHERE id= 3;
+UPDATE groceries
+SET aisle= 12 WHERE id= 4;
+UPDATE groceries
+SET aisle= 2 WHERE id= 5;
+UPDATE groceries
+SET aisle= 4 WHERE id= 6;
+--Check to see if all the data is shown under aisle column
+SELECT aisle FROM groceries;
